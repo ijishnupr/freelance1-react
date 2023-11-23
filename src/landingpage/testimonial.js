d5592b17a8a4510e00cf1468cc3312b991ece751
jishnu pr <ijishnupr@gmail.com>
2023-11-23 09:27:34 +0530

    testimonial without get know us

--- src/landingpage/testimonial.js
+++ src/landingpage/testimonial.js
@@ -1,6 +1,59 @@
-export default function Testimonial(){
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import './Testimonial.css'
+export default class Testimonial extends Component {
 
-  return <>
   
-  </>
+  render() {
+    return (
+      <Carousel
+        showArrows={false}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={2000}
+        
+       
+      >
+        <div>
+          <img src={image} />
+          <div className="myCarousel">
+            <h3>Shirley Fultz</h3>
+            <h4>Designer</h4>
+            <p>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <img src={image} alt="image" />
+          <div className="myCarousel">
+            <h3>Daniel Keystone</h3>
+            <h4>Designer</h4>
+            <p>
+              The simple and intuitive design makes it easy for me use. I highly
+              recommend Fetch to my peers.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <img src={image} />
+          <div className="myCarousel">
+            <h3>Theo Sorel</h3>
+            <h4>Designer</h4>
+            <p>
+              I enjoy catching up with Fetch on my laptop, or on my phone when
+              I'm on the go!
+            </p>
+          </div>
+        </div>
+      </Carousel>
+    );
+  }
 }
+const image='https://mydmi.imgix.net/AddisonRae.png?fm=jpg&ixlib=php-3.3.1&q=40&w=652&s=b086f4cb5b251efcae9f8dbea97e1627';
